5ba3480d345fb3fcb0faf74b5738ad190cc500f1
Henric Trotzig <henric.trotzig@brigade.com>
2016-04-23 14:29:24 +0200

    Switch binary from Ruby to Node powered
    Now that the import-js npm module is almost ready, we can switch to it.
    The biggest difference from the old binary is that commands are invoked
    through a positional argument. The way unresolved imports are handled
    has also changed. Instead of replaying the command with selections made,
    you call `importjs add` directly.
    
    I've updated documentation to instruct people to install the node
    module. I expect us to bump version to 1.0.0 when we release the npm
    module, so I've used that version in the docs.

--- import-js.py
+++ import-js.py
@@ -10,18 +10,18 @@ def no_executable_error(executable):
     return dedent('''
         Couldn't find executable {executable}.
 
-        Make sure you have the `import_js` gem installed (`gem install
-        import_js`).
+        Make sure you have the `importjs` binary installed (`npm install
+        import-js -g`).
 
         If it is installed but you still get this message, you might have to
         set a custom `executable` in your ImportJS package user settings.
         Example:
 
         {{
-            "executable": "/Users/USERNAME/.rbenv/shims/import-js"
+            "executable": "/usr/local/bin/importjs"
         }}
 
-        To see where import-js was installed, run `which import-js`
+        To see where importjs binary is located, run `which importjs`
         from the command line in your project's root.
         '''.format(executable=executable)).strip()
 
@@ -37,30 +37,27 @@ class ImportJsCommand(sublime_plugin.TextCommand):
         current_file_contents = self.view.substr(
             sublime.Region(0, self.view.size()))
 
-        environment = {
+        environment = dict(os.environ).copy()
+        environment.update({
             'LC_ALL': 'en_US.UTF-8',
             'LC_CTYPE': 'UTF-8',
-            'LANG': 'en_US.UTF-8'
-        }
+            'LANG': 'en_US.UTF-8',
+            'PATH': environment.get('PATH') + ':/usr/local/bin'
+        })
         project_root = self.view.window().extract_variables()['folder']
         settings = sublime.load_settings('ImportJS.sublime-settings')
 
         executable = settings.get('executable')
-        command = [executable]
+        cmd = args.get('command')
+        command = [executable, cmd]
 
-        if(args.get('word')):
+        if(cmd == 'word' or cmd == 'goto'):
             word = self.view.substr(self.view.word(self.view.sel()[0]))
-            command.append('--word')
             command.append(word)
 
-        if(args.get('goto')):
-            command.append('--goto')
+        if(cmd == 'add'):
+            command.append(json.dumps(args.get('imports')))
 
-        if(args.get('selections')):
-            command.append('--selections')
-            command.append(','.join(args.get('selections')))
-
-        command.append('--stdin-file-path')
         command.append(self.view.file_name())
 
         print(command)
@@ -79,6 +76,7 @@ class ImportJsCommand(sublime_plugin.TextCommand):
             raise e
 
         result = proc.communicate(input=current_file_contents.encode('utf-8'))
+        stdout = result[0].decode()
         stderr = result[1].decode()
 
         if(proc.returncode > 0):
@@ -86,52 +84,53 @@ class ImportJsCommand(sublime_plugin.TextCommand):
                 'Error when executing import-js:\n\n' + stderr)
             return
 
-        if(len(stderr) > 0):
-            meta = json.loads(stderr)
-            if(meta.get('messages')):
-                sublime.status_message(meta.get('messages'))
-            if(meta.get('ask_for_selections')):
-                def handle_selections_done(selections):
-                    if (len(selections)):
-                        self.rerun(edit, args, selections)
-                self.ask_for_selections(meta.get('ask_for_selections'),
-                                        handle_selections_done)
-                return
+        if(len(stdout) == 0):
+            sublime.error_message(
+                'Nothing returned when executing import-js:\n\n' + stderr)
+            return
 
-        stdout = result[0].decode()
-        if(args.get('goto')):
-            if(len(stdout.rstrip()) > 0):
-                self.view.window().open_file(
-                    self.project_path() + '/' + stdout.rstrip())
+        result = json.loads(stdout)
+        if(result.get('messages')):
+            sublime.status_message('\n'.join(result.get('messages')))
+        if(result.get('unresolvedImports')):
+            def handle_resolved_imports(resolved_imports):
+                args['command'] = 'add'
+                args['imports'] = resolved_imports
+                self.run(edit, **args)
+            self.ask_to_resolve(result.get('unresolvedImports'),
+                                handle_resolved_imports)
+            return
+
+        if(cmd == 'goto'):
+            self.view.window().open_file(
+                self.project_path() + '/' + result.get('goto'))
         else:
-            self.view.run_command("import_js_replace", {"characters": stdout})
-
-    def rerun(self, edit, args, selections):
-        args['selections'] = selections
-        self.run(edit, **args)
+            self.view.run_command("import_js_replace",
+                {"characters": result.get('fileContent')})
 
     def project_path(self):
         for folder in self.view.window().project_data().get('folders'):
             if(self.view.file_name().startswith(folder.get('path'))):
                 return folder.get('path')
 
-    def ask_for_selections(self, selections, on_selections_done):
-        selected = []
-        selections_iter = iter(selections)
+    def ask_to_resolve(self, unresolved_imports, on_resolve):
+        resolved = {}
+        unresolved_iter = iter(unresolved_imports)
 
-        def ask_recurse(selection):
-            if (not(selection)):
-                on_selections_done(selected)
+        def ask_recurse(word):
+            if (not(word)):
+                on_resolve(resolved)
                 return
 
             def on_done(i):
                 if(i > -1):
-                    selected.append(selection.get('word') + ':' + str(i))
-                ask_recurse(next(selections_iter, None))
+                    resolved[word] = unresolved_imports[word][i]['importPath']
+                ask_recurse(next(unresolved_iter, None))
 
             self.view.show_popup_menu(
-              selection.get('alternatives'),
-              on_done
+                list(map(lambda imp: imp.get('displayName'),
+                    unresolved_imports[word])),
+                on_done
             )
 
-        ask_recurse(next(selections_iter, None))
+        ask_recurse(next(unresolved_iter, None))
